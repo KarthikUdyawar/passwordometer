cb2f7e64794917f637bab22065343ff7e7acda84
Karthik Udyawar <karthikajitudy@gmail.com>
2023-07-01 12:55:21 +0530

    Update exception.py
    Code Formatting

--- src/middleware/exception.py
+++ src/middleware/exception.py
@@ -1,3 +1,7 @@
+"""
+Custom exception class and helper functions for exception
+handling and logging.
+"""
 import sys
 from typing import Any
 
@@ -5,6 +9,8 @@ from src.middleware.logger import logger
 
 
 class CustomException(Exception):
+    """Custom exception class for handling specific exceptions."""
+
     def __init__(self, error_message: Any, error_detail: Any):
         self.error_message = error_message_detail(
             error_message, error_detail=error_detail
@@ -15,13 +21,18 @@ class CustomException(Exception):
 
 
 def error_message_detail(error: Any, error_detail: Any) -> str:
+    """Format and log error details,
+    and return the error messages a string."""
     _, _, exc_tb = error_detail.exc_info()
     if exc_tb is None:
         raise CustomException(error, sys) from error
     file_name = exc_tb.tb_frame.f_code.co_filename
     logger.error(
-        f"""Error occurred in python script name [{file_name}]
-        line number [{exc_tb.tb_lineno}] error message [{str(error)}]"""
+        "Error occurred in python script name [%s] "
+        "line number [%d] error message [%s]",
+        file_name,
+        exc_tb.tb_lineno,
+        str(error),
     )
     return str(error)
 
@@ -32,4 +43,3 @@ if __name__ == "__main__":
     except Exception as e:
         logger.debug("Test CustomException")
         raise CustomException(e, sys) from e
-    
