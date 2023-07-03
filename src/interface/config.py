aa8a2a013a6b4b5ceda9912a83602d2fd03c1804
Karthik Udyawar <karthikajitudy@gmail.com>
2023-07-03 17:40:15 +0530

    Removed sample_data_path and sample_collection_name

--- src/interface/config.py
+++ src/interface/config.py
@@ -12,7 +12,6 @@ class FilePathConfig:
     train_data_path: str = os.path.join("artifacts", "train.csv")
     test_data_path: str = os.path.join("artifacts", "test.csv")
     raw_data_path: str = os.path.join("raw_dataset", "rockyou.txt")
-    sample_data_path: str = os.path.join("raw_dataset", "sample.txt")
     preprocessor_path: str = os.path.join("artifacts", "preprocessor.pkl")
     model_path: str = os.path.join("artifacts", "model.pkl")
 
@@ -24,4 +23,3 @@ class MongoDBConfig:
     mongodb_connection_string: str = "mongodb://localhost:27017/"
     database_name: str = "passwordometer"
     collection_name: str = "password_dataset"
-    sample_collection_name: str = "sample"
