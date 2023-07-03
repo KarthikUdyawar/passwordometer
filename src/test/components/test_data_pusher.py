0dec0432f73cc9756cd2ed98a545cc386de23839
Karthik Udyawar <karthikajitudy@gmail.com>
2023-07-03 17:40:43 +0530

    Code formatting

--- src/test/components/test_data_pusher.py
+++ src/test/components/test_data_pusher.py
@@ -1,50 +1,80 @@
+"""
+Test module for the DataPusher component.
+"""
 import os
+from dataclasses import dataclass
+
 import pandas as pd
 import pytest
-from src.interface.config import FilePathConfig, MongoDBConfig
 from src.components.data_pusher import DataPusher
-from dataclasses import dataclass
+from src.interface.config import FilePathConfig
+from src.interface.config import MongoDBConfig
 
 
 # Mock the necessary dependencies
 @dataclass
-class MockMongoDBConfig(FilePathConfig):
+class MockMongoDBConfig(MongoDBConfig):
+    """Mock configuration class for MongoDB."""
+
     mongodb_connection_string: str = "mongodb://localhost:27017/"
     database_name: str = "passwordometer"
     collection_name: str = "sample"
 
 
 @dataclass
-class MockFilePathConfig(MongoDBConfig):
+class MockFilePathConfig(FilePathConfig):
+    """Mock configuration class for file paths."""
+
     raw_data_path: str = os.path.join("raw_dataset", "sample.txt")
 
 
-@pytest.fixture
-def data_pusher_instance():
+@pytest.fixture(scope="session", name="data_pusher")  # type: ignore
+def data_pusher_fixture() -> DataPusher:
+    """Fixture for creating a DataPusher instance with mock configurations.
+
+    Returns:
+        DataPusher: The DataPusher instance.
+    """
     data_pusher = DataPusher()
-    data_pusher.filepath_config = MockFilePathConfig()
     data_pusher.mongodb_config = MockMongoDBConfig()
+    data_pusher.filepath_config = MockFilePathConfig()
+
     return data_pusher
 
 
-def test_initiate_data_push(data_pusher_instance):
-    data_pusher_instance.initiate_data_push()
+def test_initiate_data_push(data_pusher: DataPusher) -> None:
+    """Test case for initiating the data push.
+
+    Args:
+        data_pusher (DataPusher): The DataPusher instance.
+    """
+    data_pusher.initiate_data_push()
 
 
-def test_get_data_from_mongodb(data_pusher_instance):
-    df = data_pusher_instance.get_data_from_mongodb()
-    assert isinstance(df, pd.DataFrame)
-    assert df.shape[0] > 0
+def test_get_data_from_mongodb(data_pusher: DataPusher) -> None:
+    """Test case for getting data from MongoDB.
+
+    Args:
+        data_pusher (DataPusher): The DataPusher instance.
+    """
+    dataframe = data_pusher.get_data_from_mongodb()
+    assert isinstance(dataframe, pd.DataFrame)
+    assert dataframe.shape[0] > 0
 
 
-def test_push_to_mongodb(data_pusher_instance):
+def test_push_to_mongodb(data_pusher: DataPusher) -> None:
+    """Test case for pushing data to MongoDB.
+
+    Args:
+        data_pusher (DataPusher): The DataPusher instance.
+    """
     data_frame = pd.DataFrame(
         {
             "password": ["123456", "password"],
             "strength": [0.17233083320907958, 0.24954265948891105],
         },
     )
-    data_pusher_instance.push_to_mongodb(data_frame)
+    data_pusher.push_to_mongodb(data_frame)
 
 
 if __name__ == "__main__":
