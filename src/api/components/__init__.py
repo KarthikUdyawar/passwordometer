9b3860f70b8bc76db5903b16f6daf8653e2af85d
Karthik Udyawar <karthikajitudy@gmail.com>
2023-07-19 16:31:15 +0530

    Create predict_component

---
+++ src/api/components/__init__.py
@@ -0,0 +1,52 @@
+"""This module defines a FastAPI endpoint for predicting the strength of a given password using a trained machine learning model and a data pipeline."""
+
+import sys
+from src.pipe.pipeline import Pipeline
+from src.interface.config import CustomData
+from src.middleware.exception import CustomException
+from src.api.schema import PasswordPredictionRequest, PasswordPredictionResponse
+from fastapi import HTTPException
+from src.middleware.logger import logger
+from src.utils.data_validation import is_valid_password
+
+
+def predict_component(request: PasswordPredictionRequest) -> PasswordPredictionResponse:
+    """
+    Predict the strength of a given password.
+
+    Args:
+        request (PasswordPredictionRequest): The request containing the password.
+
+    Returns:
+        PasswordPredictionResponse: The response containing the password and its strength prediction.
+        
+    Raises:
+        HTTPException: If the password is invalid or any other custom exception occurs.
+    """
+    try:
+        logger.info("Called predict function")
+        custom_data = CustomData()
+        pipeline = Pipeline()
+        password = request.password
+
+        if is_valid_password(password) == 0:
+            if len(password) < 4 or len(password) > 64:
+                logger.error("400 Bad Request: Invalid password length")
+                raise HTTPException(
+                    status_code=400,
+                    detail="Length of the password should be between 4 to 64",
+                )
+            logger.error("400 Bad Request: Invalid password characters")
+            raise HTTPException(
+                status_code=400,
+                detail="Invalid password: Valid values for passwords include numerals, capital letters, !, @, #, $, %, ^, &, or *. No other special characters are allowed.",
+            )
+
+        password_df = custom_data.data2df(password)
+        strength = pipeline.predict(password_df)
+        value = custom_data.array2data(strength)
+        logger.info("200 OK: Done predict function")
+        return PasswordPredictionResponse(password=password, strength=value)
+    except CustomException as error:
+        logger.error(error, sys)
+        raise HTTPException(status_code=500, detail=error) from error
